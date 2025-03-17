3428815890b56447640c8402d851db04f2c55127
Denys Navitnii <den.navitniy@gmail.com>
2025-03-17 22:04:43 +0200

    Add DomainReadModels

---
+++ backend/CarRentalService/CarRentalService.Infrastructure/EF/Models/RentalPointReadModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CarRentalService.Infrastructure.EF.Models
+{
+    internal class RentalPointReadModel
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ backend/CarRentalService/CarRentalService.Infrastructure/EF/Models/ReservationReadModel.cs
@@ -0,0 +1,24 @@
+﻿namespace CarRentalService.Infrastructure.EF.Models
+{
+    internal class ReservationReadModel
+    {
+        public Guid Id { get; set; }
+
+        public UserReadModel User { get; set; }
+        public string UserId { get; set; }
+
+        public VehicleReadModel Vehicle { get; set; }
+        public Guid VehicleId { get; set; }
+
+        public RentalPointReadModel PickupPoint { get; set; }
+        public Guid PickupPointId { get; set; }
+
+        public RentalPointReadModel ReturnPoint { get; set; }
+        public Guid ReturnPointId { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime? ReturnedDate { get; set; }
+        public string Status { get; set; }
+    }
+}
