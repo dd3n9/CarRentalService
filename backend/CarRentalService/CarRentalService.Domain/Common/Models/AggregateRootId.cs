94f33f1daaf5f5651e4cee23c056e3dc2390c11c
Denys Navitnii <den.navitniy@gmail.com>
2025-03-17 11:14:55 +0200

    Add ddd models

---
+++ backend/CarRentalService/CarRentalService.Domain/Common/Models/AggregateRootId.cs
@@ -0,0 +1,16 @@
+﻿namespace CarRentalService.Domain.Common.Models
+{
+    public abstract record AggregateRootId<TId>
+    {
+        public TId Value { get; protected set; }
+
+        protected AggregateRootId(TId value)
+        {
+            if (value is null)
+            {
+                throw new EmptyAggregateRootIdException();
+            }
+            Value = value;
+        }
+    }
+}
