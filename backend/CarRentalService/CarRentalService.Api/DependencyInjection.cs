9f4dded711b3dd73afc795a56e6f6d37e3b57552
Denys Navitnii <den.navitniy@gmail.com>
2025-03-19 15:05:19 +0200

    Add SwaggerConfig

--- backend/CarRentalService/CarRentalService.Api/DependencyInjection.cs
+++ backend/CarRentalService/CarRentalService.Api/DependencyInjection.cs
@@ -2,6 +2,7 @@
 using CarRentalService.Api.Infrastructure;
 using Mapster;
 using MapsterMapper;
+using Microsoft.OpenApi.Models;
 using System.Reflection;
 
 namespace CarRentalService.Api
@@ -11,13 +12,14 @@ namespace CarRentalService.Api
         public static IServiceCollection AddPresentation(this IServiceCollection services)
         {
             services.AddExceptionHandler<GlobalExceptionHandler>();
-            services.AddVersionApiSetup();
-            services.AddMappings();
+            services.AddVersionApiSetup()
+           .    AddMappings()
+                .AddSwagger();
 
             return services;
         }
 
-        private static void AddVersionApiSetup(this IServiceCollection services)
+        private static IServiceCollection AddVersionApiSetup(this IServiceCollection services)
         {
             services.AddApiVersioning(o =>
             {
@@ -34,6 +36,8 @@ namespace CarRentalService.Api
                 options.GroupNameFormat = "'v'VW";
                 options.SubstituteApiVersionInUrl = true;
             });
+
+            return services;
         }
         private static IServiceCollection AddMappings(this IServiceCollection services)
         {
@@ -44,6 +48,38 @@ namespace CarRentalService.Api
             services.AddScoped<IMapper, ServiceMapper>();
             return services;
         }
-
+        private static IServiceCollection AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(option =>
+            {
+                option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+                option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter a valid token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+                option.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+                            Reference = new OpenApiReference
+                            {
+                                Type=ReferenceType.SecurityScheme,
+                                Id="Bearer"
+                            }
+                        },
+                        new string[]{}
+                    }
+                });
+            });
+            return services;
+        }
     }
 }
